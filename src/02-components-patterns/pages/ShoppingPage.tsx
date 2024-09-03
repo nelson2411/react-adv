01ce07ea4c5efd57824abf51c0f757e9c42c92a0
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2024-09-03 18:02:45 +0200

    chore: Update code formatting and file structure

---
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "../../styles/styles.module.css"
+import { ProductCard } from "../components/ProductCard"
+
+function ShoppingPage() {
+  return (
+    <div>
+      <h1>Shopping Page</h1>
+      <hr />
+      <ProductCard />
+    </div>
+  )
+}
+
+export default ShoppingPage
